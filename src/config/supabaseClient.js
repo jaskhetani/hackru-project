6ebd64b626d47cfe2108eb1dea4564dd645bfae6
Jas Khetani <jaskhetani1981@gmail.com>
2024-10-26 20:41:27 -0400

    modified:   .env 	modified:   src/config/supabaseClient.js

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mikikrgdulllacswawkp.supabase.co'
+const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
